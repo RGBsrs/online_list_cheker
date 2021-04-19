6becab9b34f73e994a19fb360ab4289bdb91cabf
RGBSrs <grjanko.d.a@gmail.com>
2021-04-19 10:01:40 +0300

    delete unused variables

--- server/settings.py
+++ server/settings.py
@@ -8,8 +8,6 @@ class Config():
     SECRET_KEY = "B\xb2?.\xdf\x9f\xa7m\xf8\x8a%,\xf7\xc4\xfa\x91"
     MAX_CONTENT_LENGTH = 50 * 1024 * 1024
 
-    IMAGE_UPLOADS = "/home/username/app/app/static/images/uploads"
-
     SESSION_COOKIE_SECURE = True
 
 class ProductionConfig(Config):
@@ -20,7 +18,6 @@ class DevelopmentConfig(Config):
     ENV ='development'
 
     UPLOAD_FOLDER = os.path.join(BASEDIR,'uploads')
-    IMAGE_UPLOADS = "/home/username/projects/my_app/app/static/images/uploads"
 
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Ghjws733@localhost/online-checker'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
